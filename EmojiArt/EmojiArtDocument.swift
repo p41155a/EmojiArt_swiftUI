71dfd0b09c5e54b8ebfebcbff56398485ca78c00
p41155a <50395024+p41155a@users.noreply.github.com>
2021-02-15 18:07:31 +0900

    👨🏼‍🏫 Lecture 7: Multithreading EmojiArt

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,65 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Yoojin Park on 2021/02/15.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    static let palette: String = "⭐️☁️🍎🍋🥝🍗🍔🍟"
+    
+    @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    @Published private(set) var backgroundImage: UIImage?
+    
+    var emojis: [EmojiArt.Emoji] { emojiArt.emojis }
+    
+    // MARK: Intent(s)
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackgroundURL(_ url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+        fetchBackgroundImageData()
+    }
+    
+    private func fetchBackgroundImageData() {
+        backgroundImage = nil
+        if let url = self.emojiArt.backgroundURL {
+            DispatchQueue.global(qos: .userInitiated).async {
+                if let imageData = try? Data(contentsOf: url) { // 타임아웃과 같은 오류 대비
+                    DispatchQueue.main.async {
+                        // backgroundImage가 publish임으로 바뀌면 view가 다시 그려질 것이지만 현재 global
+                        // main이 아닌 곳에서 무언가를 그릴 수 없기 때문에 main으로 바꿈
+                        if url == self.emojiArt.backgroundURL {
+                            // 이미지를 끌어오고 로딩되기 전에 다른 이미지를 끌어오고 다른 이미지가 끈 후에 전에 있던 이미지가 오는 이상한 형태 방지를 위한 if문
+                            self.backgroundImage = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension EmojiArt.Emoji {
+    var fontSize: CGFloat { CGFloat(self.size) }
+    var location: CGPoint { CGPoint(x: CGFloat(x), y: CGFloat(y)) }
+}
